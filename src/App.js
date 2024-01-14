5cbeb056066dc0e62aa4a56b1e3117bd3f758361
Mila <142541687+MilaBorodkina@users.noreply.github.com>
2024-01-14 17:05:08 +0100

    Footer added

--- src/App.js
+++ src/App.js
@@ -2,5 +2,22 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Designed & Built by{" "}
+        <a href="https://portfolio-mila-borodkina.netlify.app/" target="_blank">
+          Mila Borodkina
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/MilaBorodkina/react-weather-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
