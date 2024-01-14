f0c46edf1013960fe78a45a915eaa787b18f49bb
Mila <142541687+MilaBorodkina@users.noreply.github.com>
2024-01-14 17:06:14 +0100

    Footer updated

--- src/App.js
+++ src/App.js
@@ -7,13 +7,18 @@ export default function App() {
       Weather App
       <footer>
         Designed & Built by{" "}
-        <a href="https://portfolio-mila-borodkina.netlify.app/" target="_blank">
+        <a
+          href="https://portfolio-mila-borodkina.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Mila Borodkina
         </a>{" "}
         and is{" "}
         <a
           href="https://github.com/MilaBorodkina/react-weather-project"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
